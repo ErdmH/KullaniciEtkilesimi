d3782e6366b63ea74ea1ebd380514f3babd98772
ErdmH <erdmkucuk@gmail.com>
2023-12-06 20:52:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kullanicietkilesimi/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.kullanicietkilesimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var buttonPopup:Button = findViewById(R.id.buttonPopup)
+        var popupMenu = PopupMenu(this@MainActivity, buttonPopup)
+        popupMenu.menuInflater.inflate(R.menu.menu1, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+
+            when(item.itemId){
+                R.id.menuItemSil -> {
+                    Toast.makeText(applicationContext, "Sil Seçildi", Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.menuItemDuzenle -> {
+                    Toast.makeText(applicationContext, "Düzenle Seçildi", Toast.LENGTH_LONG).show()
+                    true
+                }else -> false
+            }
+        }
+        buttonPopup.setOnClickListener {
+            popupMenu.show()
+        }
+    }
+}
